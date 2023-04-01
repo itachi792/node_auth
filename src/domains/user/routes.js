6e7ddff2f466021932f76aebabdf8c1bca14b478
itachi792 <singhavichal45@gmail.com>
2023-04-02 02:43:55 +0530

    implement login with access token

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router=express.Router();
-const { createNewUser } = require("./controller");
+const { createNewUser,authenticateUser } = require("./controller");
+
+//Signin
+router.post("/",async(req,res)=>{
+    try {
+        let {email , password}=req.body;
+        email=email.trim();
+        password=password.trim();
+
+        if(!(email&&password)){
+            throw Error("Empty credentials supplied!");
+        }
+
+        const authenticatedUser = await authenticateUser({email,password});
+
+        res.status(200).json(authenticatedUser);
+        
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
+
 //Signup
 router.post("/signup",async(req,res)=>{
     try {
